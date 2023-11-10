f83490d68796975053353f32197392018ea84938
Björn Petersen <git@bjoernpetersen.net>
2023-11-10 13:44:50 +0100

    chore: Add 'validate-renovate-config / validate' default status check
    BREAKING CHANGE: This adds a new required status check to the default
    list. Add the following job to your projects to include this job:
    ```yaml
    validate-renovate-config:
      uses: BlindfoldedSurgery/renovate-config/.github/workflows/validate.yml@main
    ```

--- variables.tf
+++ variables.tf
@@ -75,6 +75,7 @@ variable "required_status_checks" {
     "lint / lint",
     "test / test",
     "build-container-image / build",
+    "validate-renovate-config / validate",
   ]
   description = "The list of status checks that need to pass for PRs"
 }
